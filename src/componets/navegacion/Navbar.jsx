868d971fbef6a76fd3ac5c1e5faf8e10657d1d1a
wanda <wanda.gagliero@gmail.com>
2022-08-21 13:20:33 -0300

    ramanavbar

---
+++ src/componets/navegacion/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+
+ export  class Navbar extends React.Component  {
+  render (){
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a className="link" herf="">Inicio</a>
+          </li>
+          <li>
+            <a className="link" herf="">Nosotros</a>
+          </li>
+          <li>
+            <a className="link" herf="">Productos</a>
+          </li>
+          <li>
+            <a className="link" herf="">Contactos</a>
+          </li>
+        </ul>
+  
+      </nav>
+    )
+  }
+ 
+  
+}
